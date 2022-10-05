ec6bd424f988d9033c33c8199e5ace0f0abca39a
Thiago Santos <91476713+Thmsantos@users.noreply.github.com>
2022-10-05 17:18:32 -0300

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,25 +1,19 @@
 const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
 var bodyParser = require('body-parser')
 
-//Carregando módulos
+
 const index_Routes = require('./routes/index_Routes')
 
-//Chama a o express criando uma nova aplicação dentro da variavel app
-const app = express()
-
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
-app.use(bodyParser.json())
 
-//Rotas
+app.use(bodyParser.json())
 
 app.use('/cinema', index_Routes)
 
-//Inicializa Servidor
-const port = 3000
+const port = 3001
 app.listen(port, () => {
-    console.log(`essa porra ta rodando no http://localhost:3000`)
+    console.log(`essa porra ta rodando no http://localhost:3001`)
 })
-
-/* node ejs */
