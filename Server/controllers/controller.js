bde675514e1b6b45ccbcfc10d7ae0905407cc458
Thiago Santos <91476713+Thmsantos@users.noreply.github.com>
2022-10-11 17:02:57 -0300

    Update controller.js

--- Server/controllers/controller.js
+++ Server/controllers/controller.js
@@ -20,26 +20,6 @@ class teste{
       res.status(200).json(dados)
     }
 
-    static async idFilme(req, res){
-      let index = req.params.id
-      await database.sync()
-      let dados = await models.findAll({
-        where: {id: index},
-
-        attributes: [
-          'id',
-          'Sala',
-          'Filme',
-          'Duracao',
-          'Horario',
-          'Ingressos_Restantes',
-          'Sinopse'
-        ]
-        
-      })
-      
-      res.status(200).json(dados)
-    }
 }
 
 module.exports = teste;
